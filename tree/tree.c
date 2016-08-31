fafa067cafba45166b923b0055b9878b66d56f95
ed <qiujia145@163.com>
2016-09-01 01:09:12 +0800

    update

--- tree/tree.c
+++ tree/tree.c
@@ -21,12 +21,94 @@ SearchTree Find(ElementType e, SearchTree t)
 		return t;
 }
 
-S
+SearchTree FindMin(SearchTree t)
+{
+	if (t->left == NULL)
+		return t;
+	return FindMin(t->left);
+}	
 
+SearchTree FindMax(SearchTree t)
+{
+	if (t->right == NULL)
+		return t;
+	return FindMax(t->right);
+}	
 
+SearchTree Insert(ElementType e, SearchTree t)
+{
+	if (t == NULL)
+	{
+		t = malloc(sizeof(struct TreeNode));
+		if (t = NULL)
+			fatal_error("out of memory");
+		t->element = e;
+		t->left = NULL;
+		t->right = NULL;
+	}
+	if (t->element < e)
+		t->right = Insert(e, t->right);
+	else if (e < t->element)
+		t->left = Insert(e, t->left);
 
+	return t;	
+}
 
-
-
-
+SearchTree Delete(ElementType e, SearchTree t)
+{
+	SearchTree tmp;
+	if (t == NULL)
+		fatal_error("node not exist.");
+	if (t->element < e)
+		t->right = Delete(e, t->right);
+	else if (t->element > e)
+		t->left = Delete(e, t->left);
+	else
+	{
+		if (!t->left && !t->right)
+		{
+			free(t);
+			t = NULL;
+		}
+		else if (t->left && !t->right)
+		{
+			tmp = t;
+			t = t->left;
+			free(tmp);
+		}
+		else if (!t->left && t->right)
+		{
+			tmp = t;
+			t = t->right;
+			free(tmp);
+		}
+		else
+		{
+			tmp = FindMin(t->right);
+			t->element = tmp->element;
+			t->right = Delete(t->element, t->right);
+		}
+	}
 	
+	return t;	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
