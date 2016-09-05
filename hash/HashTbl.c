c63cba3bdae9641a73a3a0c001b356504bf9071c
ed <qiujia145@163.com>
2016-09-05 18:51:53 +0800

    complete Hashtable

--- hash/HashTbl.c
+++ hash/HashTbl.c
@@ -27,9 +27,111 @@ HashTable InitializeTable(int size)
 	return ht;
 }
 
+Position Find(ElementType key, HashTable h)
+{
+	Position p;
+	List l;
+		
+	l = h->lists[Hash(key, h->size)];
+	p = l->next;
+	while (p != NULL && p->e != key)
+		p = p->next;
+	return p;
+}
+
+void Insert(ElementType key, HashTable h)
+{
+	Position p;
+	List l;
+
+	p = Find(key, h);
+	if (p == NULL)
+	{
+		l = h->lists[Hash(key, h->size)];
+		p = malloc(sizeof(struct ListNode));
+		if (p == NULL)
+			fatal_error("out of memory.");
+		p->next = l->next;
+		p->element = key;
+		l->next = p;
+	}	
+}
+
+void Delete(ElementType key, HashTable h)
+{
+	Position p, tmp;
+	List l;
+	
+	l = h->lists[Hash(key, h->size)];
+	p = l;
+	while(p->next != NULL && p->next->e != key)
+		p = p->next;
+	if (p->next != NULL)
+	{
+		tmp = p->next;
+		p->next = tmp->next;
+		free(tmp);	
+	}
+}
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
 
 
--- hash/HashTbl.h
+++ hash/HashTbl.h
@@ -25,6 +25,3 @@ struct HashTbl
 
 
 
-
-
-
